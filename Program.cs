8aa29c9ecbcb5a9e8665c2bb4ee35134fe97c953
Brunagarstka <bruna.luisa.garstkas2@gmail.com>
2024-09-02 19:46:00 -0300

    atualizações02

--- Program.cs
+++ Program.cs
@@ -3,7 +3,34 @@ using c_propriedades_metodos_construtores.Models;
 using System.Globalization;
 using System.Data;
 
-new ExemploExceção().Metodo1();
+Dictionary<string, string> estados = new Dictionary<string, string>();
+estados.Add("SP", "São Paulo");
+estados.Add("RJ", "Rio de Janeiro");
+estados.Add("MG", "Minas Gerais");
+
+foreach(KeyValuePair<string, string> item in estados) //pode se utilizar var no lugar de KeyValuePair
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+Console.WriteLine("----------------");
+estados.Remove("MG");
+estados["SP"] = "São Paulo - valor alterado";
+foreach(KeyValuePair<string, string> item in estados) //pode se utilizar var no lugar de KeyValuePair
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+Console.WriteLine("----------------");
+string chave = "RJ";
+Console.WriteLine($"Verificando o elemento: {chave}");
+if(estados.ContainsKey(chave))
+{
+    Console.WriteLine($"Valor existente: {chave}");
+}
+else 
+{
+    Console.WriteLine($"Valor não existe. É seguro adicionar a chave: {chave}");
+}
+Console.WriteLine(estados["RJ"]);
 
 
 
@@ -13,6 +40,54 @@ new ExemploExceção().Metodo1();
 
 
 
+/*
+Stack<int> pilha = new Stack<int>();
+pilha.Push(4);
+pilha.Push(6);
+pilha.Push(8);
+pilha.Push(10);
+foreach(int item in pilha)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine($"Removendo o elemento do topo: {pilha.Pop()}");
+pilha.Push(20);
+foreach(int item in pilha)
+{
+    Console.WriteLine(item);
+}
+*/
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+/*
+Queue<int> fila = new Queue<int>();
+fila.Enqueue(2);
+fila.Enqueue(4);
+fila.Enqueue(6);
+fila.Enqueue(8);
+foreach(int item in fila)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+fila.Enqueue(10);
+foreach(int item in fila)
+{
+    Console.WriteLine(item);
+}
+*/
 
 
 
@@ -22,6 +97,7 @@ new ExemploExceção().Metodo1();
 
 
 
+/*new ExemploExceção().Metodo1();*/
 
 
 
